93a020a3c28318a45363f03644fa6060a08b42f8
dtgiang251 <giang251@gmail.com>
2025-09-16 16:07:23 +0700

    update

--- app/[locale]/(marketing)/blog/[slug]/page.tsx
+++ app/[locale]/(marketing)/blog/[slug]/page.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-
+import { type Metadata } from "next";
 import { BlogLayout } from "@/components/blog-layout";
 import fetchContentType from "@/lib/strapi/fetchContentType";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
+import { generateMetadataObject } from '@/lib/shared/metadata';
 import ClientSlugHandler from "../../ClientSlugHandler";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const pageData = await fetchContentType('articles', {
+    filters: { locale: params.locale },
+    populate: "seo.metaImage",
+  }, true)
+
+  const seo = pageData?.seo;
+  const metadata = generateMetadataObject(seo);
+  return metadata;
+}
 
 export default async function SingleArticlePage({
   params,

0487c2192baadcaafcd9c198fb270cd7bc0e6bed
dtgiang251 <giang251@gmail.com>
2025-12-01 21:49:46 +0700

    favicon

--- lib/shared/metadata.ts
+++ lib/shared/metadata.ts
@@ -1,13 +1,18 @@
 import { strapiImage } from '../strapi/strapiImage';
 
 export function generateMetadataObject(seo: any) {
+  //console.log('SEO data:', seo); // thêm dòng này để debug
+  //console.log('metaImage:', seo?.metaImage); // kiểm tra metaImage
+  
   return {
     title: seo?.metaTitle || 'Default Title', // Fallback to 'Default Title' if title is not provided
     description: seo?.metaDescription || 'Default Description', // Fallback to 'Default Description'
     openGraph: {
       title: seo?.ogTitle || seo?.metaTitle || 'Default OG Title',
       description: seo?.ogDescription || seo?.metaDescription || 'Default OG Description',
-      images: seo?.metaImage ? [{ url: strapiImage(seo?.metaImage.url) }] : [],
+      images: seo?.metaImage?.url 
+        ? [{ url: strapiImage(seo.metaImage.url), width: seo.metaImage.width, height: seo.metaImage.height }] 
+        : [],
     },
     twitter: {
       card: seo?.twitterCard || 'summary_large_image',

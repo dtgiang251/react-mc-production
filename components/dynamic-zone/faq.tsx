1801e52c2af519abf15f425a65930062338704ac
dtgiang251 <giang251@gmail.com>
2025-12-17 21:53:42 +0700

    update

--- components/dynamic-zone/faq.tsx
+++ components/dynamic-zone/faq.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export const FAQ = ({ heading, sub_heading, faqs, image }: { heading: string, sub_heading: string, faqs: any[], image: { url: string } }) => {
   return (
     <>
-    <section className="bg-secondary pt-20 relative"> 
+    <section className="bg-secondary py-20 relative"> 
     <div id="faqs" className="absolute -top-[200px]"></div>
       <Container className="pr-0 px-5 md:pr-0 md:px-0">
         <div className="flex flex-col md:flex-row">
